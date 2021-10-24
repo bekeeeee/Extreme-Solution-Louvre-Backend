6697a2b2abdfa47c290ddb39f86211937808f54b
bekeeeee <m.ragab9521@gmail.com>
2021-10-24 15:00:51 +0200

    add swagger file

--- src/web/application.ts
+++ src/web/application.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
-
+import * as swagger from "swagger-express-ts";
+import { SwaggerDefinitionConstant } from "swagger-express-ts";
 import cookieSession from "cookie-session";
 
 import { InversifyExpressServer } from "inversify-express-utils";
@@ -27,93 +28,108 @@ import { ArtsRepository } from "../data/art/arts.repository";
 import { ArtsService } from "../logic/services/arts.service";
 import { BadRequestError, ValidationException } from "../logic/exceptions";
 
-
-
 export class App extends Application {
   constructor() {
     super({
       containerOpts: {
-        defaultScope: 'Singleton',
+        defaultScope: "Singleton",
       },
       morgan: {
         mode: MorganMode.DEV,
       },
-    })
+    });
   }
-  private server: any
+  private server: any;
   configureServices(container: Container): void {
-    container.bind(DBContext).toSelf()
-    container.bind(ArtsRepository).toSelf()
-    container.bind(ArtsService).toSelf()
-    container.bind(UsersRepository).toSelf()
-    container.bind(UsersService).toSelf()
+    container.bind(DBContext).toSelf();
+    container.bind(ArtsRepository).toSelf();
+    container.bind(ArtsService).toSelf();
+    container.bind(UsersRepository).toSelf();
+    container.bind(UsersService).toSelf();
   }
 
   async setup(options: IAbstractApplicationOptions) {
-    const _db = this.container.get(DBContext)
+    const _db = this.container.get(DBContext);
 
-    await _db.connect()
+    await _db.connect();
 
-    this.server = new InversifyExpressServer(this.container)
+    this.server = new InversifyExpressServer(this.container);
 
     this.server.setErrorConfig((app: any) => {
       app.use((err: any, req: Request, res: Response, next: NextFunction) => {
         if (err instanceof ValidationException) {
-          const response = BaseHttpResponse.failed(err.message, 422)
-          return res.status(response.statusCode).json(response)
+          const response = BaseHttpResponse.failed(err.message, 422);
+          return res.status(response.statusCode).json(response);
         }
 
-
         if (err instanceof NotAuthenticated) {
-          const response = BaseHttpResponse.failed(err.message, 401)
-          return res.status(response.statusCode).json(response)
+          const response = BaseHttpResponse.failed(err.message, 401);
+          return res.status(response.statusCode).json(response);
         }
 
         if (err instanceof NotAuthorized) {
-          const response = BaseHttpResponse.failed(err.message, 401)
-          return res.status(response.statusCode).json(response)
+          const response = BaseHttpResponse.failed(err.message, 401);
+          return res.status(response.statusCode).json(response);
         }
 
         if (err instanceof BadRequestError) {
-          const response = BaseHttpResponse.failed(err.message, 400)
-          return res.status(response.statusCode).json(response)
+          const response = BaseHttpResponse.failed(err.message, 400);
+          return res.status(response.statusCode).json(response);
         }
-
-
 
         if (err instanceof Error) {
-          const response = BaseHttpResponse.failed(err.message, 500)
-          return res.status(response.statusCode).json(response)
+          const response = BaseHttpResponse.failed(err.message, 500);
+          return res.status(response.statusCode).json(response);
         }
 
-        next()
-      })
-    })
+        next();
+      });
+    });
 
     this.server.setConfig((app: any) => {
-      app.use(express.json())
-      app.use(morgan(options.morgan.mode))
+      app.use(express.json());
+      app.use(morgan(options.morgan.mode));
       // cookieSession cookie-session stores the session data on the client within a cookie
       // express-session stores only a session identifier on the client within a cookie and stores the session data on the server, typically in a database.
       app.use(
         cookieSession({
-          name: 'jwt',
+          name: "jwt",
           signed: false,
           secure: false,
         })
-      )
-    })
+      );
 
-    const app = this.server.build()
+      app.use("/api-docs/swagger", express.static("swagger"));
+      app.use(
+        "/api-docs/swagger/assets",
+        express.static("node_modules/swagger-ui-dist")
+      );
+      app.use(
+        swagger.express({
+          definition: {
+            info: {
+              title: "My api",
+              version: "1.0",
+            },
+            externalDocs: {
+              url: "My url",
+            },
+            // Models can be defined here
+          },
+        })
+      );
+    });
+
+    const app = this.server.build();
 
     app.listen(process.env.PORT, () => {
-      console.log(`server is running on http://localhost:${process.env.PORT}`)
-    })
+      console.log(`server is running on http://localhost:${process.env.PORT}`);
+    });
   }
 
   get getserver() {
-    return this.server
+    return this.server;
   }
 }
 
-new App()
+new App();
--- src/web/controllers/art/arts.controller.ts
+++ src/web/controllers/art/arts.controller.ts
@@ -1,12 +1,21 @@
-import { CreateArtDto, GetOneArtDto, UpdateArtDto } from "../../../logic/dto/arts";
+import {
+  CreateArtDto,
+  GetOneArtDto,
+  UpdateArtDto,
+} from "../../../logic/dto/arts";
 import { ArtsService } from "../../../logic/services/arts.service";
 import { BaseHttpResponse } from "../../lib/base-http-response";
 import { AuthorizedToAdmin } from "../../middlewares/Authorized-to-admin";
 import { CurrentUserMiddleware } from "../../middlewares/current-user.middleware";
 import { ValidateRequestMiddleware } from "../../middlewares/validate-request.middleware";
 import { Request, Response } from "express";
-import { controller, httpDelete, httpGet, httpPatch, httpPost } from "inversify-express-utils";
-
+import {
+  controller,
+  httpDelete,
+  httpGet,
+  httpPatch,
+  httpPost,
+} from "inversify-express-utils";
 
 @controller("/api/v1/art")
 export class ArtsController {
@@ -23,39 +32,39 @@ export class ArtsController {
     res.status(response.statusCode).json(response);
   }
 
-  @httpGet('/', CurrentUserMiddleware)
+  @httpGet("/", CurrentUserMiddleware)
   async index(req: Request, res: Response) {
-    const arts = await this._service.all()
+    const arts = await this._service.all();
 
-    const response = BaseHttpResponse.success(arts)
-    res.json(response)
+    const response = BaseHttpResponse.success(arts);
+    res.json(response);
   }
 
   @httpPatch(
-    '/:id',
+    "/:id",
     CurrentUserMiddleware,
     AuthorizedToAdmin,
     ValidateRequestMiddleware.withParams(UpdateArtDto)
   )
   async edit(req: Request, res: Response) {
-    const art = await this._service.updateOne(req.body)
+    const art = await this._service.updateOne(req.body);
 
-    const response = BaseHttpResponse.success(art, 200)
+    const response = BaseHttpResponse.success(art, 200);
 
-    res.status(response.statusCode).json(response)
+    res.status(response.statusCode).json(response);
   }
 
   @httpDelete(
-    '/:id',
+    "/:id",
     CurrentUserMiddleware,
     AuthorizedToAdmin,
     ValidateRequestMiddleware.withParams(GetOneArtDto)
   )
   async destroy(req: Request, res: Response) {
-    await this._service.deleteOne(req.body)
+    await this._service.deleteOne(req.body);
 
-    const response = BaseHttpResponse.success({}, 200)
+    const response = BaseHttpResponse.success({}, 200);
 
-    res.status(response.statusCode).json(response)
+    res.status(response.statusCode).json(response);
   }
 }
